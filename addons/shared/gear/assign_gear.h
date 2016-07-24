e0d94415a1c2288e3ace90b46f0cb8f5285ec971
freghar <freghar@dummy.tld>
2016-07-24 04:08:29 +0200

    shared: provide empty-respawn versions of ASSIGN/ADD_GEAR
    Signed-off-by: freghar <freghar@dummy.tld>

--- addons/shared/gear/assign_gear.h
+++ addons/shared/gear/assign_gear.h
@@ -9,3 +9,15 @@
     magazines[] += {_mags}; respawnMagazines[] += {_mags}; \
     items[] += {_items}; respawnItems[] += {_items}; \
     linkedItems[] += {_linked}; respawnLinkedItems[] += {_linked}
+
+#define ASSIGN_GEAR_EMPTY_RESPAWN \
+    weapons[] = {_weaps}; respawnWeapons[] = {}; \
+    magazines[] = {_mags}; respawnMagazines[] = {}; \
+    items[] = {_items}; respawnItems[] = {}; \
+    linkedItems[] = {_linked}; respawnLinkedItems[] = {}
+
+#define ADD_GEAR_EMPTY_RESPAWN \
+    weapons[] += {_weaps}; respawnWeapons[] = {}; \
+    magazines[] += {_mags}; respawnMagazines[] = {}; \
+    items[] += {_items}; respawnItems[] = {}; \
+    linkedItems[] += {_linked}; respawnLinkedItems[] = {}
