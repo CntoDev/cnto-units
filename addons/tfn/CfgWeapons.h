2ccd458cc211d35994ab3fcc5664875c9083d675
freghar <freghar@dummy.tld>
2016-08-14 06:14:38 +0200

    us,tfn: use vanilla flashlights
    Signed-off-by: freghar <freghar@dummy.tld>

--- addons/tfn/CfgWeapons.h
+++ addons/tfn/CfgWeapons.h
@@ -3,25 +3,25 @@
 class hlc_rifle_SAMR;
 class cnto_tfn_samr_flash : hlc_rifle_SAMR {
     scope = 1;
-    class LinkedItems { side(RH_SFM952V); };
+    class LinkedItems { side(acc_flashlight); };
 };
 
 class rhs_weap_hk416d10_LMT;
 class cnto_tfn_416_flash : rhs_weap_hk416d10_LMT {
     scope = 1;
-    class LinkedItems { side(RH_SFM952V); };
+    class LinkedItems { side(acc_flashlight); };
 };
 
 class rhs_weap_hk416d145_m320;
 class cnto_tfn_416_gl_flash : rhs_weap_hk416d145_m320 {
     scope = 1;
-    class LinkedItems { side(RH_SFM952V); };
+    class LinkedItems { side(acc_flashlight); };
 };
 
 class hlc_m249_SQuantoon;
 class cnto_tfn_m249 : hlc_m249_SQuantoon {
     scope = 1;
-    class LinkedItems { side(RH_SFM952V); };
+    class LinkedItems { side(acc_flashlight); };
 };
 
 class tf47_m3maaws;
--- addons/us/CfgWeapons.h
+++ addons/us/CfgWeapons.h
@@ -3,19 +3,19 @@
 class rhs_weap_m4a1_carryhandle_pmag;
 class cnto_us_m4a1_flash : rhs_weap_m4a1_carryhandle_pmag {
     scope = 1;
-    class LinkedItems { side(RH_SFM952V); };
+    class LinkedItems { side(acc_flashlight); };
 };
 
 class rhs_weap_m4a1_carryhandle_m203S;
 class cnto_us_m4a1_gl_flash : rhs_weap_m4a1_carryhandle_m203S {
     scope = 1;
-    class LinkedItems { side(RH_SFM952V); };
+    class LinkedItems { side(acc_flashlight); };
 };
 
 class rhs_weap_m249_pip_L;
 class cnto_us_m249_flash : rhs_weap_m249_pip_L {
     scope = 1;
-    class LinkedItems { side(RH_SFM952V); };
+    class LinkedItems { side(acc_flashlight); };
 };
 
 class tf47_m3maaws;
