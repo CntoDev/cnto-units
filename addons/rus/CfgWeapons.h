f15dc6ef34df61d321b1756c3f8d74b8912e3f67
CNTODachi <dene_ford@yahoo.com>
2016-08-17 20:37:37 +0200

    Add the rus faction

---
+++ addons/rus/CfgWeapons.h
@@ -0,0 +1,19 @@
+#include "\cnto\units\shared\weap_linked.h"
+
+class rhs_weap_ak74m_camo_npz;
+class cnto_rus_ak74m_flash : rhs_weap_ak74m_camo_npz {
+    scope = 1;
+    class LinkedItems { side(rhs_acc_2dpZenit); };
+};
+
+class rhs_weap_ak74m_gp25_npz;
+class cnto_rus_ak74m_gl : rhs_weap_ak74m_gp25_npz {
+    scope = 1;
+    class LinkedItems { side(rhs_acc_dtk3); };
+};
+
+class rhs_weap_rpg7;
+class cnto_rus_rpg7_optic : rhs_weap_rpg7 {
+    scope = 1;
+    class LinkedItems { optic(rhs_acc_pgo7v); };
+};
