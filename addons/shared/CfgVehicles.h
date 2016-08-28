f90bfd7b43c67a561c7f81b0053c2c30c51d856e
freghar <freghar@dummy.tld>
2016-08-28 16:45:23 +0200

    shared: update scopebox to rhacc-less version
    Signed-off-by: freghar <freghar@dummy.tld>

--- addons/shared/CfgVehicles.h
+++ addons/shared/CfgVehicles.h
@@ -22,34 +22,23 @@ class cnto_shared_scopebox : Land_PlasticCase_01_small_F {
     editorSubcategory = CNTO_Misc;
     displayName = "Scopebox";
     class UserActions {
-        ADD_SCOPE(optic_ACO_grn,     "<t color='#e05200'>ACO Green</t>");
-        ADD_SCOPE(optic_Aco,         "<t color='#e05200'>ACO Red</t>");
-        ADD_SCOPE(optic_ACO_grn_smg, "<t color='#e05200'>ACO SMG Green</t>");
-        ADD_SCOPE(optic_Aco_smg,     "<t color='#e05200'>ACO SMG Red</t>");
+        ADD_SCOPE(optic_Aco,         "<t color='#f00000'>ACO Red</t>");
+        ADD_SCOPE(optic_ACO_grn,     "<t color='#f00000'>ACO Green</t>");
+        ADD_SCOPE(optic_Aco_smg,     "<t color='#f00000'>ACO SMG Red</t>");
+        ADD_SCOPE(optic_ACO_grn_smg, "<t color='#f00000'>ACO SMG Green</t>");
 
-        ADD_SCOPE(RH_compM2,      "<t color='#008ee0'>Aimpoint CompM2</t>");
-        //ADD_SCOPE(RH_compM21,     "<t color='#008ee0'>Aimpoint CompM2 Low</t>");
-        ADD_SCOPE(RH_compM2_tan,  "<t color='#008ee0'>Aimpoint CompM2 Tan</t>");
-        //ADD_SCOPE(RH_compM21_tan, "<t color='#008ee0'>Aimpoint CompM2 Tan Low</t>");
+        ADD_SCOPE(rhsusf_acc_compm4, "<t color='#0000f0'>Aimpoint M68 CCO</t>");
+        ADD_SCOPE(rhsusf_acc_M2A1,   "<t color='#0000f0'>Milkor M2A1 GL Sight</t>");
 
-        ADD_SCOPE(RH_t1,         "<t color='#e05200'>Aimpoint T1</t>");
-        ADD_SCOPE(RH_t1_tan,     "<t color='#e05200'>Aimpoint T1 Tan</t>");
-        ADD_SCOPE(RH_barska_rds, "<t color='#e05200'>Barska RDS</t>");
-        ADD_SCOPE(RH_cmore,      "<t color='#e05200'>C-More RDS</t>");
+        ADD_SCOPE(optic_holosight,           "<t color='#00f0f0'>Mk17 Holosight</t>");
+        ADD_SCOPE(optic_Holosight_blk_F,     "<t color='#00f0f0'>Mk17 Holosight (Black)</t>");
+        ADD_SCOPE(optic_Holosight_khk_F,     "<t color='#00f0f0'>Mk17 Holosight (Khaki)</t>");
+        ADD_SCOPE(optic_holosight_smg,       "<t color='#00f0f0'>Mk17 Holosight SMG</t>");
+        ADD_SCOPE(optic_Holosight_smg_blk_F, "<t color='#00f0f0'>Mk17 Holosight SMG (Black)</t>");
 
-        ADD_SCOPE(RH_eotech553,     "<t color='#008ee0'>Eotech 553</t>");
-        ADD_SCOPE(RH_eotech553_tan, "<t color='#008ee0'>Eotech 553 Tan</t>");
-        ADD_SCOPE(RH_LTdocter,      "<t color='#008ee0'>Larue Docter</t>");
-        //ADD_SCOPE(RH_LTdocter1,     "<t color='#008ee0'>Larue Docter low</t>");
-
-        ADD_SCOPE(rhsusf_acc_eotech_552, "<t color='#e05200'>M552 CCO</t>");
-        ADD_SCOPE(rhsusf_acc_compm4,     "<t color='#e05200'>M68 CCO</t>");
-        ADD_SCOPE(optic_holosight,       "<t color='#e05200'>Mk17 Holosight</t>");
-        ADD_SCOPE(optic_holosight_smg,   "<t color='#e05200'>Mk17 Holosight SMG</t>");
-
-        //ADD_SCOPE(RH_refles,         "<t color='#008ee0'>Trijicon Reflex</t>");
-        ADD_SCOPE(rhsusf_acc_eotech, "<t color='#008ee0'>XPS3</t>");
-        ADD_SCOPE(RH_zpoint,         "<t color='#008ee0'>Zeiss Z-point</t>");
+        ADD_SCOPE(rhsusf_acc_eotech_552,   "<t color='#00f000'>Eotech M552 CCO</t>");
+        ADD_SCOPE(rhsusf_acc_eotech_552_d, "<t color='#00f000'>Eotech M552 CCO (Desert)</t>");
+        ADD_SCOPE(rhsusf_acc_eotech_xps3,  "<t color='#00f000'>Eotech XPS3 (dim)</t>");
     };
     ace_cargo_canLoad = 0;
     ace_dragging_canCarry = 0;
