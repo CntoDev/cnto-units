b17d7792bde3c3951cf740082896c3dcc72a3cf7
freghar <freghar@dummy.tld>
2016-09-24 16:11:06 +0200

    us,tfn,col: switch back to Surefire flashlight
    Vanilla flashlight, with or without custom config changes,
    has locally displayed light intensity bugs when used by other
    players or AI - the first ~3 seconds are super intensive.
    
    This is a mild issue with vanilla flashlight config, but a major
    one with custom one as the high intensity lights up a good 200-300
    meters ahead with intense light.
    
    Signed-off-by: freghar <freghar@dummy.tld>

--- addons/col/CfgWeapons.h
+++ addons/col/CfgWeapons.h
@@ -15,7 +15,7 @@ class cnto_col_g3_gl_flash : HLC_Rifle_g3ka4_GL {
 class rhs_weap_m249_pip_S_vfg;
 class cnto_col_m249_flash : rhs_weap_m249_pip_S_vfg {
     scope = 1;
-    class LinkedItems { side(acc_flashlight); };
+    class LinkedItems { side(RH_SFM952V); };
 };
 
 class rhs_weap_rpg7;
--- addons/tfn/CfgWeapons.h
+++ addons/tfn/CfgWeapons.h
@@ -3,25 +3,25 @@
 class hlc_rifle_SAMR;
 class cnto_tfn_samr_flash : hlc_rifle_SAMR {
     scope = 1;
-    class LinkedItems { side(acc_flashlight); };
+    class LinkedItems { side(RH_SFM952V); };
 };
 
 class rhs_weap_hk416d10_LMT;
 class cnto_tfn_416_flash : rhs_weap_hk416d10_LMT {
     scope = 1;
-    class LinkedItems { side(acc_flashlight); };
+    class LinkedItems { side(RH_SFM952V); };
 };
 
 class rhs_weap_hk416d145_m320;
 class cnto_tfn_416_gl_flash : rhs_weap_hk416d145_m320 {
     scope = 1;
-    class LinkedItems { side(acc_flashlight); };
+    class LinkedItems { side(RH_SFM952V); };
 };
 
 class rhs_weap_m249_pip_S_vfg;
 class cnto_tfn_m249_flash : rhs_weap_m249_pip_S_vfg {
     scope = 1;
-    class LinkedItems { side(acc_flashlight); };
+    class LinkedItems { side(RH_SFM952V); };
 };
 
 class tf47_m3maaws;
